e2d6b46334eed1f71fd75af826aed23520ef6817
relusion <vladimir.gondarev@gmail.com>
2024-07-23 07:48:14 +0200

    refactor: Update BingSearch class to format search results for RAG

--- SimpleAIQueryBot/Tools/BingSearch.cs
+++ SimpleAIQueryBot/Tools/BingSearch.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System.Text.Json;
 
 namespace SimpleAIQueryBot.Tools
@@ -16,7 +17,7 @@ namespace SimpleAIQueryBot.Tools
 
         public BingSearch()
         {
-            _subscriptionKey = Environment.GetEnvironmentVariable("BING_SEARCH_API_KEY") 
+            _subscriptionKey = Environment.GetEnvironmentVariable("BING_SEARCH_API_KEY")
                 ?? throw new InvalidOperationException("Bing API key is missing or empty.");
 
             _httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _subscriptionKey);
@@ -33,12 +34,13 @@ namespace SimpleAIQueryBot.Tools
 
                 var contentString = await response.Content.ReadAsStringAsync();
 
-                var jsonDocument = JsonDocument.Parse(contentString);
-                var webPages = jsonDocument.RootElement
-                    .GetProperty("webPages")
-                    .GetProperty("value");
+                // var jsonDocument = JsonDocument.Parse(contentString);
+                // var webPages = jsonDocument.RootElement
+                //     .GetProperty("webPages")
+                //     .GetProperty("value");
 
-                return ExtractResults(webPages);
+                var formattedResults = FormatResultsForRAG(contentString);
+                return formattedResults;
             }
             catch (Exception e)
             {
@@ -50,9 +52,51 @@ namespace SimpleAIQueryBot.Tools
         private static string BuildQueryString(string searchQuery)
         {
             var queryString = QueryParameter + Uri.EscapeDataString(searchQuery);
-            queryString += MktParameter + "en-us";
-            queryString += TextDecorationsParameter + bool.TrueString;
+            // queryString += MktParameter + "en-us";
+            // queryString += TextDecorationsParameter + bool.TrueString;
             return queryString;
+        }
+
+        static string FormatResultsForRAG(string jsonResult)
+        {
+            using JsonDocument doc = JsonDocument.Parse(jsonResult);
+            var root = doc.RootElement;
+            var webPages = root.GetProperty("webPages").GetProperty("value");
+
+            var sb = new StringBuilder();
+            sb.AppendLine("BEGIN_SEARCH_RESULTS");
+
+            foreach (var page in webPages.EnumerateArray())
+            {
+                sb.AppendLine("BEGIN_RESULT");
+                sb.AppendLine($"TITLE: {page.GetProperty("name").GetString()}");
+                sb.AppendLine($"URL: {page.GetProperty("url").GetString()}");
+                sb.AppendLine($"SNIPPET: {page.GetProperty("snippet").GetString()}");
+                sb.AppendLine("END_RESULT");
+            }
+
+            sb.AppendLine("END_SEARCH_RESULTS");
+
+            return sb.ToString();
+        }
+
+        static string FormatResults(string jsonResult)
+        {
+            using JsonDocument doc = JsonDocument.Parse(jsonResult);
+            var root = doc.RootElement;
+            var webPages = root.GetProperty("webPages").GetProperty("value");
+
+            var sb = new StringBuilder();
+
+            foreach (var page in webPages.EnumerateArray())
+            {
+                sb.AppendLine($"Title: {page.GetProperty("name").GetString()}");
+                sb.AppendLine($"URL: {page.GetProperty("url").GetString()}");
+                sb.AppendLine($"Snippet: {page.GetProperty("snippet").GetString()}");
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
         }
 
         private static string ExtractResults(JsonElement webPages)
